66605ef4be7733752ca92d275eb5338948f9ffc6
Gaurav Pratap Singh <gs492979@gmail.com>
2025-02-19 01:22:36 +0530

    animation added on btn pressed & game start on keyboard press func added.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let gamePattern = [];
 let userClickedPattern = [];
 let buttonColors = ["red", "blue", "green", "yellow"];
+let gameStarted = false;
 
 function nextSequence() {
   let randomNumber = Math.floor(Math.random() * 4);
@@ -16,10 +17,27 @@ allBtn.forEach((btn) => {
   btn.addEventListener("click", handleClick);
 });
 
+//for starting game
+document.addEventListener("keydown", () => {
+  if (!gameStarted) {
+    nextSequence();
+    gameStarted = true;
+  }
+});
+// Handle user click
 function handleClick() {
   let userChosenColor = this.getAttribute("id");
   userClickedPattern.push(userChosenColor);
   playSound(userChosenColor);
+  animatePress(userChosenColor);
+}
+
+//animation on btn pressed
+function animatePress(currentColor) {
+  document.getElementById(`${currentColor}`).classList.add("pressed");
+  setInterval(() => {
+    document.getElementById(`${currentColor}`).classList.remove("pressed");
+  }, 100);
 }
 
 // Flash animation on btn
